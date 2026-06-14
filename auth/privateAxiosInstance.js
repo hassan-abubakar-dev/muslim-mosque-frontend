c746e89b53a913d4cec591815ba6a21c0391a1fa
Hassan Abubakar <hassan@example.com>
2026-06-14 14:33:49 +0100

    increase timeout of axios instances

--- auth/privateAxiosInstance.js
+++ auth/privateAxiosInstance.js
@@ -7,7 +7,7 @@ const isDev = import.meta.env.VITE_ENV === 'development';
 const privateAxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: { 'Content-Type': 'application/json' },
-    timeout: 40000,
+    timeout: 10000,
     withCredentials: true,
 });
 
--- auth/publicAxiosInstance.js
+++ auth/publicAxiosInstance.js
@@ -6,7 +6,7 @@ const publicAxiosInstance = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
-    timeout: 20000,
+    timeout: 10000,
     withCredentials: true,
 });
 
